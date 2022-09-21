ec87d809a1fcb464855f650481b343ea26189ba4
osoroderrick <osoroderrick@yahoo.com>
2022-09-20 22:23:53 -0400

    Homework #2

---
+++ Account.java
@@ -0,0 +1,23 @@
+package homework2;
+
+public class Account {
+
+    private double balance;
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if(balance > 0) {
+            this.balance = balance;
+        } else {
+            this.balance = 0;
+        }
+    }
+
+
+
+}
+
---
+++ Main.java
@@ -0,0 +1,18 @@
+package homework2;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    Account acc1 = new Account();
+    acc1.setBalance(-200);
+    System.out.println(acc1.getBalance());
+
+
+
+
+
+
+    }
+}
